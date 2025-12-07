dae63d13fc60ab42c6e39c9a53199ac8ae6d4f38
dejan.music <dejan.music@DESKTOP-0CIFSD6>
2025-12-07 18:35:58 +0100

    removing some initial tryouts

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
 
-#from django.template import loader
-
 from .models import Question, Choice
 
 # Create your views here.
@@ -14,33 +12,14 @@ def index(request):
     context = { "latest_question_list": latest_question_list }
     return render(request, "polls/index.html", context)
 
-    # LONGER SOLUTION WITH LOADING TEMPLATE
-    # load template
-    # template = loader.get_template("polls/index.html")
-    # prepare context with variables you will use in your template
-    # output = ",".join([q.question_text for q in latest_question_list])
-    # render template in HttpResponse
-    # return HttpResponse(template.render(context, request))
-
 def detail(request, question_id):
     # shorter and compact version
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question })
-    ### Longer version
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    
-    return render(request, "polls/detail.html", {"question": question})
-    #HttpResponse("You are looking at question %s." % question_id)
-    ###
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", { "question": question })
-    #response = "You are looking at the results of question %s."
-    #return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -62,5 +41,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a user hits
         # the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
-
-    #return HttpResponse("You are voting on question %s." % question_id)
