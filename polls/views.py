e5f9b4ad01a7a26836ab7f51c913515429d00110
dejan.music <dejan.music@DESKTOP-0CIFSD6>
2025-12-08 06:39:15 +0100

    add generic views concept and simplify code

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = { "latest_question_list": latest_question_list }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    # to tell django which template to use, default would be <app name>/<model name>_list.html
+    # so polls/question_list.html
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    # shorter and compact version
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question })
+    def get_queryset(self):
+        """ Return the last five published questions. """
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", { "question": question })
+
+class DetailView(generic.DetailView):
+    # Since we are using a model, we do not need to specify explicitly, the name of context variable matches to
+    # model name
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
