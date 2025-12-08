b52eb43d0a3b0a0a4082bc9557941a3cb6282ecc
dejan.music <dejan.music@DESKTOP-0CIFSD6>
2025-12-09 00:18:09 +0100

    introduct new tests and adapt views to properly deal with future polls

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -15,8 +16,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """ Return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions.
+        Not including those set to be published in the future.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -24,6 +28,12 @@ class DetailView(generic.DetailView):
     # model name
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Exclude any questions that are not published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
